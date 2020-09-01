a1045898017486298b9cead598625ac49e5a3ab9
lapo <lapo.bisognin@stud.unifi.it>
2020-09-01 13:46:17 +0200

    Aggiunta implementazione slot updating e redoneSum, modifica segnale updated. Modifica attributo sum

--- Sum.cpp
+++ Sum.cpp
@@ -4,3 +4,35 @@
 
 #include "Sum.h"
 Sum::Sum(QWidget* parent): QPushButton(parent){}
+
+void Sum::updating(const double &value, const QString &string, const int &row, const int &column) {
+    auto iter=values.find(row+column*10);
+    if(string=="")
+    {
+        if(iter!=values.end())
+        {
+            values.erase(iter);
+        }
+    }
+    else
+    {
+        if(iter==values.end())
+            values.insert(std::pair<int,int>(row+column*10,value));
+        else
+            iter->second=value;
+    }
+    if(values.empty())
+        sum="";
+    else
+    {
+        sum=0;
+        for(auto iterator:values)
+        {
+            sum=sum.toDouble()+iterator.second;
+        }
+    }
+}
+
+void Sum::redoneSum(bool f){
+    emit updated(sum);
+}
--- Sum.h
+++ Sum.h
@@ -5,8 +5,19 @@
 #ifndef SPREADSHEET_SUM_H
 #define SPREADSHEET_SUM_H
 #include <QPushButton>
+#include <QVariant>
+
 class Sum: public QPushButton{
 public:
     explicit Sum(QWidget* parent=nullptr);
+    ~Sum(){};
+public slots:
+    void updating(const double &value, const QString &string, const int &row, const int &column);
+    void redoneSum(bool f);
+public:
+    std::map<int, double> values;
+    QVariant sum;
+signals:
+    void updated(QVariant &number);
 };
 #endif //SPREADSHEET_SUM_H
