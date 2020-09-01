3b6a1b8c4866c15738846129e3d28f741514c8da
lapo <lapo.bisognin@stud.unifi.it>
2020-09-01 13:21:49 +0200

    Aggiunta classe Sum

---
+++ Sum.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lapo on 01/09/20.
+//
+
+#include "Sum.h"
---
+++ Sum.h
@@ -0,0 +1,11 @@
+//
+// Created by lapo on 01/09/20.
+//
+
+#ifndef SPREADSHEET_SUM_H
+#define SPREADSHEET_SUM_H
+#include <QPushButton>
+class Sum: public QPushButton{
+
+};
+#endif //SPREADSHEET_SUM_H
