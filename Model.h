1cf3035d2e738118ff75c79ac4ccbcb1e6cc69d0
lapo <lapo.bisognin@stud.unifi.it>
2020-09-09 10:41:41 +0200

    Eliminazione valore di default da metodo "setData" in Model.h

--- Model.h
+++ Model.h
@@ -19,7 +19,7 @@ public:
     int rowCount(const QModelIndex &parent) const override;
     int columnCount(const QModelIndex &parent) const override;
     QVariant data(const QModelIndex &index, int role) const override;
-    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
     Qt::ItemFlags flags(const QModelIndex &index) const override;
     QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
     std::map <int,QVariant> strings;
